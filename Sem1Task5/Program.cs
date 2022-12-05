6f3334a0aef10ac5c2c455dc55ff1dd3850e1872
PodorogaElla <Elka_forest@mail.ru>
2022-12-05 14:00:15 +0500

    add solution for task 0,1,3,5,7

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтоьы данные были не пустыми
+if(inputLine!=null)
+{
+
+    //Парсим введенное выражение
+    int inputNumber = int.Parse(inputLine);
+
+    int startNumber = inputNumber*(-1);
+
+    //Выводим значение
+    string outLine = string.Empty;
+
+    while(startNumber<inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber++;
+        //startNumber=startNumber+1;
+    }
+    //outLine = outLine + inputNumber;
+    outLine = outLine + inputNumber;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outLine);  
+
+
+}
---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтоьы данные были не пустыми
+if(inputLine!=null)
+{
+    //Парсим введенное выражение
+    int inputNumber = int.Parse(inputLine);
+
+    //Выводим остаток от деления
+    int lastDigit = inputNumber % 10;
+
+    //Выводим данные в консоль
+    Console.WriteLine(lastDigit);  
+
+}
